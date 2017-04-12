10837b23380cb40ab7dc500c0d51821df0137a90
Scylol <djd3dobbin@gmail.com>
2017-04-12 18:03:44 -0400

    working version

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
+let nextPageToken;
+
+// getDataFromApi({q: "cats"}, callback)
+// getDataFromApi({q: "cats", pageToken: nextPageToken}, callback)
 
 function getDataFromApi(searchTerm, callback) {
+
     let query = {
     part: "snippet",
     key: "AIzaSyDD8n-uCr_ls7qk7aSqEFeSH7scMdZrr1k",
     q: searchTerm,
     type: "video"
-    // nextPageToken: "",
-    // prevPageToken: ""
+  }
+  $.getJSON(YOUTUBE_BASE_URL, query, callback);
+}
 
+function getNext(searchTerm, callback) {
+    let query = {
+    pageToken:nextPageToken,
+    part: "snippet",
+    key: "AIzaSyDD8n-uCr_ls7qk7aSqEFeSH7scMdZrr1k",
+    q: searchTerm,
+    type: "video"
   }
   $.getJSON(YOUTUBE_BASE_URL, query, callback);
 }
@@ -17,15 +30,17 @@ function getDataFromApi(searchTerm, callback) {
 function displayYouTubeSearchData(data) {
   console.log(data);
   let resultElement = '';
-  let nextButton = ``
+  nextPageToken = data.nextPageToken;
+
+  
   if (data.items) {
     data.items.forEach(function(item) {
       
      // resultElement += `<li><a href = "https://www.youtube.com/watch?v=${item.id.videoId}"><img src='${item.snippet.thumbnails.medium.url}'/></a>`+
      //                    `<a href = "https://www.youtube.com/watch?v=${item.id.videoId}"><p>${item.snippet.title}</p></a>` +
      //                    `<a href = "https://www.youtube.com/channel/${item.snippet.channelId}"><p>Channel:${item.snippet.channelTitle}</p></li>`;
-        resultElement += `<li><a href = "#${item.id.videoId}"><img src="${item.snippet.thumbnails.medium.url}"/></a>`+
-                          `<a href = "#_" class="lightbox" id="${item.id.videoId}"><iframe width="640" height="480" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe></a>`+
+        resultElement += `<li><a href = "#${item.id.videoId}"><img src="${item.snippet.thumbnails.medium.url}"/></a>
+                          <a href = "#_" class="lightbox" id="${item.id.videoId}"><iframe width="640" height="480" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe></a>`+
                            `<a href = "https://www.youtube.com/channel/${item.snippet.channelId}"><p>Channel:${item.snippet.channelTitle}</p></li>`;
 
                           
@@ -37,14 +52,25 @@ function displayYouTubeSearchData(data) {
   }
 
   $('.js-search-results ul').html(resultElement);
+
+
 }
 
-function watchSubmit() {
+$(function(){
+  
   $('.js-search-form').submit(function(e) {
     e.preventDefault();
     let query = $(this).find('.js-query').val();
     getDataFromApi(query, displayYouTubeSearchData);
   });
-}
 
-$(function(){watchSubmit();});
+
+  $('#next-btn').on('click',function(){
+    let query = $('.js-query').val();
+    console.log(query);
+    getNext(query, displayYouTubeSearchData);
+  });
+    
+
+});
+
