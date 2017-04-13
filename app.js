757af193b87c56034d342c1348338bf0f9ffdecf
Scylol <djd3dobbin@gmail.com>
2017-04-12 20:57:50 -0400

    final version

--- app.js
+++ app.js
@@ -1,76 +1,62 @@
 const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3/search";
 let nextPageToken;
+let prevPageToken;
+let prevPageChecker;
 
-// getDataFromApi({q: "cats"}, callback)
-// getDataFromApi({q: "cats", pageToken: nextPageToken}, callback)
-
-function getDataFromApi(searchTerm, callback) {
-
-    let query = {
-    part: "snippet",
-    key: "AIzaSyDD8n-uCr_ls7qk7aSqEFeSH7scMdZrr1k",
-    q: searchTerm,
-    type: "video"
-  }
-  $.getJSON(YOUTUBE_BASE_URL, query, callback);
+function getDataFromApi(query, callback) {
+    query.part = "snippet";
+    query.key = "AIzaSyDD8n-uCr_ls7qk7aSqEFeSH7scMdZrr1k";
+    query.type = "video";
+    query.maxResults = 20;
+ 
+ $.getJSON(YOUTUBE_BASE_URL, query, callback);
 }
-
-function getNext(searchTerm, callback) {
-    let query = {
-    pageToken:nextPageToken,
-    part: "snippet",
-    key: "AIzaSyDD8n-uCr_ls7qk7aSqEFeSH7scMdZrr1k",
-    q: searchTerm,
-    type: "video"
-  }
-  $.getJSON(YOUTUBE_BASE_URL, query, callback);
-}
-
 
 function displayYouTubeSearchData(data) {
   console.log(data);
-  let resultElement = '';
+  let resultElement = "";
   nextPageToken = data.nextPageToken;
-
+  prevPageToken = data.prevPageToken;
+  prevPageChecker = data.hasOwnProperty("prevPageToken");
+  
+  if(prevPageChecker === false) {
+      $("#prev-btn").addClass("hidden");
+    }
   
   if (data.items) {
     data.items.forEach(function(item) {
-      
-     // resultElement += `<li><a href = "https://www.youtube.com/watch?v=${item.id.videoId}"><img src='${item.snippet.thumbnails.medium.url}'/></a>`+
-     //                    `<a href = "https://www.youtube.com/watch?v=${item.id.videoId}"><p>${item.snippet.title}</p></a>` +
-     //                    `<a href = "https://www.youtube.com/channel/${item.snippet.channelId}"><p>Channel:${item.snippet.channelTitle}</p></li>`;
-        resultElement += `<li><a href = "#${item.id.videoId}"><img src="${item.snippet.thumbnails.medium.url}"/></a>
-                          <a href = "#_" class="lightbox" id="${item.id.videoId}"><iframe width="640" height="480" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe></a>`+
-                           `<a href = "https://www.youtube.com/channel/${item.snippet.channelId}"><p>Channel:${item.snippet.channelTitle}</p></li>`;
-
-                          
-
-    });
+        resultElement += `<li><a href = "#${item.id.videoId}"><img class ="thumbnail-image" src="${item.snippet.thumbnails.medium.url}"/></a>
+                          <a href = "#_" class="lightbox" id="${item.id.videoId}"><iframe width="640" height="480" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe></a>
+                          <a href = "https://www.youtube.com/watch?v=${item.id.videoId}"><p class="title">${item.snippet.title}</p></a>
+                          <a href = "https://www.youtube.com/channel/${item.snippet.channelId}"><p class="channel">Channel: ${item.snippet.channelTitle}</p></li>`;
+                        });
   }
   else {
-    resultElement+= `<p>No Results</p>`
+    resultElement+= `<p>No Results</p>`;
   }
-
-  $('.js-search-results ul').html(resultElement);
-
-
-}
+    $('.js-search-results ul').html(resultElement);
+  }
 
 $(function(){
   
-  $('.js-search-form').submit(function(e) {
+  $(".js-search-form").submit(function(e) {
     e.preventDefault();
-    let query = $(this).find('.js-query').val();
-    getDataFromApi(query, displayYouTubeSearchData);
+    let query = $(this).find(".js-query").val();
+    getDataFromApi({q: query}, displayYouTubeSearchData);
+    $("#next-btn").removeClass("hidden");
+   });
+
+
+  $(".js-search-results").on("click", "#next-btn", function(){
+    let query = $(".js-query").val();
+    getDataFromApi({q: query, pageToken: nextPageToken}, displayYouTubeSearchData);
+    $("#prev-btn").removeClass("hidden");
   });
 
-
-  $('#next-btn').on('click',function(){
+  $(".js-search-results").on("click", "#prev-btn", function(){
     let query = $('.js-query').val();
-    console.log(query);
-    getNext(query, displayYouTubeSearchData);
+    getDataFromApi({q: query, pageToken: prevPageToken}, displayYouTubeSearchData);
   });
-    
-
+  
 });
 
